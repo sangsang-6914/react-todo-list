9245e4f00ab5723cf91f2adeaa0f80298d82e7f1
sangsang-6914 <sangsang6914@gmail.com>
2023-02-19 17:52:00 +0900

    불필요한 Import 코드 삭제

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Header.module.css';
 import { IoMoonSharp, IoSunnySharp } from 'react-icons/io5';
 import { useDarkMode } from '../../context/DarkModeContext';
